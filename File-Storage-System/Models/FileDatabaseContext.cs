f1fd100a196f03de3de61183a90e264df24ff26b
iamstan13y <iamstan13y@gmail.com>
2021-08-02 11:42:44 +0200

    added file db context.

---
+++ File-Storage-System/Models/FileDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_Storage_System.Models
+{
+    public class FileDatabaseContext: DbContext
+    {
+        public FileDatabaseContext(DbContextOptions<FileDatabaseContext> options)
+        :base(options)
+        {
+
+        }
+
+        public DbSet<FileUpload> FileUpload { get; set; }
+    }
+}
