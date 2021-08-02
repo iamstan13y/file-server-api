62b8f50ce3c92c8a6ba62313c995341ffc7e355b
iamstan13y <iamstan13y@gmail.com>
2021-08-02 12:00:12 +0200

    added sql server nuget package.

--- File-Storage-System/Startup.cs
+++ File-Storage-System/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
