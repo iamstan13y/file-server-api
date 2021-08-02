aaba9f4df079fe32e642596fc5cff614a6d2e2d7
iamstan13y <iamstan13y@gmail.com>
2021-08-02 10:53:52 +0200

    Added file upload controller.

---
+++ File-Storage-System/Controllers/UploadController.cs
@@ -0,0 +1,40 @@
+﻿using File_Storage_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_Storage_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        private async Task<IActionResult> UploadFile(IFormFile file, string applicationId)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory() + "\\Files\\");
+            var fileName = Path.GetFileName(file.FileName);
+            var url = Path.Combine($"{basePath}{DateTime.Now.Ticks}", fileName);
+
+                using (var stream = new FileStream(url, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                var newFile = new FileUpload
+                {
+                    FileName = fileName,
+                    Url= url,
+                    ApplicationId = applicationId,
+                    DateCreated = DateTime.Now.Date
+                };
+
+                context.FilesOnFileSystem.Add(newFile);
+                context.SaveChanges();
+            }
+        }
+    }
+}
