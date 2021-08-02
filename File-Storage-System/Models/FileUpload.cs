cc1d0876869a6ff0c86b5ca15f2c21bf81386037
iamstan13y <iamstan13y@gmail.com>
2021-08-02 10:53:27 +0200

    Updated FileUpload model.

---
+++ File-Storage-System/Models/FileUpload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_Storage_System.Models
+{
+    public class FileUpload
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string ApplicationId { get; set; }
+        public string Url { get; set; }
+        public DateTime? DateCreated { get; set; }
+
+    }
+}
