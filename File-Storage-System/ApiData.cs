49a2712678f76a68c0f1a81c79e3773462b824a0
iamstan13y <iamstan13y@gmail.com>
2021-08-02 14:40:40 +0200

    Added static data class.

---
+++ File-Storage-System/ApiData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_Storage_System
+{
+    public static class ApiData
+    {
+        public static string BaseURL {get; set;}
+    }
+}
