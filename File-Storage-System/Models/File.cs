85ed287453f9e4a02b8263afda1d088facc68bde
iamstan13y <iamstan13y@gmail.com>
2021-08-02 08:54:37 +0200

    added file model.

---
+++ File-Storage-System/Models/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_Storage_System.Models
+{
+    public class File
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string ApplicationId { get; set; }
+        public string Url { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
