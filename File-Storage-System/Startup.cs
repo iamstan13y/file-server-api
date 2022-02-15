45f8196ea5ddad3b43c80e12a09cc47b4e505b1c
iamstan13y <iamstan13y@gmail.com>
2022-02-15 16:04:34 +0200

    fixes + deployment to heroku.

--- File-Storage-System/Startup.cs
+++ File-Storage-System/Startup.cs
@@ -36,7 +36,7 @@ namespace File_Storage_System
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
