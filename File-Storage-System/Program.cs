8fa44977b08e92d764546843de99646d78999faf
iamstan13y <iamstan13y@gmail.com>
2022-02-16 17:19:12 +0200

    Added logging.

--- File-Storage-System/Program.cs
+++ File-Storage-System/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace File_Storage_System
 {
@@ -7,11 +9,31 @@ namespace File_Storage_System
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File("Logs/logs.txt", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
