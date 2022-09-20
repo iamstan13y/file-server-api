7738e28c338a9da6e412bbd2f7c948ff39a21e9b
Keith Stanley <iamstan13y@gmail.com>
2022-09-20 15:42:23 +0200

    File Server new rhetoric - II.

---
+++ FileServer.API/Models/ImageFile.cs
@@ -0,0 +1,10 @@
+﻿namespace FileServer.API.Models
+{
+    public class ImageFile
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? Path { get; set; }
+        public string? Url { get; set; }
+    }
+}
