01569b3a5e4e4be9f78b0ecaf976a69a9ac32e8e
Keith Stanley <iamstan13y@gmail.com>
2022-09-20 15:42:11 +0200

    File Server new rhetoric - II.

---
+++ FileServer.API/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FileServer.API.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ImageFile>? ImageFiles { get; set; }
+    }
+}
