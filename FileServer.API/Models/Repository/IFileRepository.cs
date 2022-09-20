402e81365e486a1a2f1274fddbb151f3a31ee9d8
Keith Stanley <iamstan13y@gmail.com>
2022-09-20 16:11:15 +0200

    File Server new rhetoric - III.

--- FileServer.API/Models/Repository/IFileRepository.cs
+++ FileServer.API/Models/Repository/IFileRepository.cs
@@ -1,11 +1,12 @@
-﻿using ModelLibrary;
+﻿using FileServer.API.Models.Data;
+using ModelLibrary;
 
 namespace FileServer.API.Models.Repository
 {
     public interface IFileRepository
     {
-        Task<Result<ImageFile>> AddAsync(ImageFile imageFile);
-        Task<Result<IEnumerable<ImageFile>>> GetAllAsync();
+        Task<Result<JFile>> AddAsync(JFile imageFile);
+        Task<Result<IEnumerable<JFile>>> GetAllAsync();
         Task<Result<bool>> DeleteAsync(string fileName);
     }
 }
