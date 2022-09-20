fda82c4d2e5edb94dffe86fbfbdc86b0c0b76150
Keith Stanley <iamstan13y@gmail.com>
2022-09-20 20:01:56 +0200

    JFile new mods & DB migrations.

---
+++ FileServer.API/Migrations/20220920180035_JFileV2ToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileServer.API.Migrations
+{
+    public partial class JFileV2ToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "FileSize",
+                table: "Files",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FileSize",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
