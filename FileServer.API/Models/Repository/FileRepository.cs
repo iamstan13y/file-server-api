1109d98ea30331d062ae15bdbb92cd947540c2f4
Keith Stanley <iamstan13y@gmail.com>
2022-09-20 15:42:17 +0200

    File Server new rhetoric - II.

---
+++ FileServer.API/Models/Repository/FileRepository.cs
@@ -0,0 +1,38 @@
+﻿using FileServer.API.Models.Data;
+using Microsoft.EntityFrameworkCore;
+using ModelLibrary;
+
+namespace FileServer.API.Models.Repository
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FileRepository(ApplicationDbContext context) => _context = context;
+
+        public async Task<Result<ImageFile>> AddAsync(ImageFile imageFile)
+        {
+            await _context.ImageFiles!.AddAsync(imageFile);
+            await _context.SaveChangesAsync();
+
+            return new Result<ImageFile>(imageFile);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(string fileName)
+        {
+            var file = await _context.ImageFiles!.Where(x => x.FileName == fileName).FirstOrDefaultAsync();
+            if (file == null) return new Result<bool>(false, new List<string> { "File record not found." });
+
+            _context.ImageFiles!.Remove(file);
+            await _context.SaveChangesAsync();
+
+            return new Result<bool>(true);
+        }
+
+        public async Task<Result<IEnumerable<ImageFile>>> GetAllAsync()
+        {
+            var files = await _context.ImageFiles!.ToListAsync();
+            return new Result<IEnumerable<ImageFile>>(files);
+        }
+    }
+}
---
+++ FileServer.API/Models/Repository/IFileRepository.cs
@@ -0,0 +1,11 @@
+﻿using ModelLibrary;
+
+namespace FileServer.API.Models.Repository
+{
+    public interface IFileRepository
+    {
+        Task<Result<ImageFile>> AddAsync(ImageFile imageFile);
+        Task<Result<IEnumerable<ImageFile>>> GetAllAsync();
+        Task<Result<bool>> DeleteAsync(string fileName);
+    }
+}
