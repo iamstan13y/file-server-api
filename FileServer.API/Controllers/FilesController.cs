d72f64584795f34f83c6300e59955ca1233a177f
Keith Stanley <iamstan13y@gmail.com>
2024-10-01 22:33:19 +0200

    Refactor FilesController for modern syntax and readability
    Simplified namespace declaration to use a semicolon.
    Refactored FilesController constructor to use expression-bodied member.
    Combined constructor and inheritance syntax for ControllerBase.
    Initialized _fileService field directly in constructor parameter list.
    Adjusted indentation and formatting of UploadFileAsync method for consistency.

--- FileServer.API/Controllers/FilesController.cs
+++ FileServer.API/Controllers/FilesController.cs
@@ -5,26 +5,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-namespace FileServer.API.Controllers
+namespace FileServer.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class FilesController(IFileService fileService) : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class FilesController : ControllerBase
+    private readonly IFileService _fileService = fileService;
+
+	[HttpPost("upload")]
+    [ProducesResponseType(typeof(Result<JFile>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Result<JFile>), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UploadFileAsync(IFormFile file)
     {
-        private readonly IFileService _fileService;
+        var result = await _fileService.UploadFileAsync(file);
 
-        public FilesController(IFileService fileService) => _fileService = fileService;
+        if (!result.Success) return BadRequest(result);
 
-        [HttpPost("upload")]
-        [ProducesResponseType(typeof(Result<JFile>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(Result<JFile>), StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UploadFileAsync(IFormFile file)
-        {
-            var result = await _fileService.UploadFileAsync(file);
-
-            if (!result.Success) return BadRequest(result);
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
