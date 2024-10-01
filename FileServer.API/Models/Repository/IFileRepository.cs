0ee61c7b52a9b6e53b3dc7d16032612fe1d86198
Keith Stanley <iamstan13y@gmail.com>
2024-10-01 22:38:23 +0200

    Refactor IFileRepository interface structure
    The IFileRepository interface has been refactored to improve readability. The interface declaration has been moved out of the nested block and is now directly under the namespace declaration. The methods AddAsync, GetAllAsync, and DeleteAsync are retained but are no longer nested within an inner interface declaration.

--- FileServer.API/Models/Repository/IFileRepository.cs
+++ FileServer.API/Models/Repository/IFileRepository.cs
@@ -3,12 +3,11 @@ using FileServer.API.Models.Local;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace FileServer.API.Models.Repository
+namespace FileServer.API.Models.Repository;
+
+public interface IFileRepository
 {
-    public interface IFileRepository
-    {
-        Task<Result<JFile>> AddAsync(JFile file);
-        Task<Result<IEnumerable<JFile>>> GetAllAsync();
-        Task<Result<bool>> DeleteAsync(string fileName);
-    }
+    Task<Result<JFile>> AddAsync(JFile file);
+    Task<Result<IEnumerable<JFile>>> GetAllAsync();
+    Task<Result<bool>> DeleteAsync(string fileName);
 }
