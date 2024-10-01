0c296b6ba8b2e289078478e91706b2dc398a0943
Keith Stanley <iamstan13y@gmail.com>
2024-10-01 22:37:45 +0200

    Refactor Result.cs for readability and consistency
    Changed namespace declaration to use a semicolon for conciseness.
    Reformatted the Result class to improve readability.
    Reordered and reformatted constructors for consistent style.
    Moved and reformatted properties for better organization.

--- FileServer.API/Models/Local/Result.cs
+++ FileServer.API/Models/Local/Result.cs
@@ -1,32 +1,31 @@
-﻿namespace FileServer.API.Models.Local
+﻿namespace FileServer.API.Models.Local;
+
+public class Result<T>
 {
-    public class Result<T>
+    public Result() { }
+
+    public Result(T Data) => this.Data = Data;
+
+    public Result(bool Success, string Message)
     {
-        public Result() { }
-
-        public Result(T Data) => this.Data = Data;
-
-        public Result(bool Success, string Message)
-        {
-            this.Success = Success;
-            this.Message = Message;
-        }
-
-        public Result(bool Success, T Data, string Message)
-        {
-            this.Success = Success;
-            this.Data = Data;
-            this.Message = Message;
-        }
-
-        public Result(T Data, string Message)
-        {
-            this.Data = Data;
-            this.Message = Message;
-        }
-
-        public bool Success { get; set; } = true;
-        public T? Data { get; set; }
-        public string? Message { get; set; }
+        this.Success = Success;
+        this.Message = Message;
     }
+
+    public Result(bool Success, T Data, string Message)
+    {
+        this.Success = Success;
+        this.Data = Data;
+        this.Message = Message;
+    }
+
+    public Result(T Data, string Message)
+    {
+        this.Data = Data;
+        this.Message = Message;
+    }
+
+    public bool Success { get; set; } = true;
+    public T? Data { get; set; }
+    public string? Message { get; set; }
 }
