e61ebaabb0c6b2650c7d66fa526431a0d5e56459
Keith Stanley <iamstan13y@gmail.com>
2024-10-01 22:36:04 +0200

    Refactor JFileV2ToDb class and update FileSize type
    Modified namespace declaration to remove an extra space.
    Changed JFileV2ToDb class from public to partial.
    Moved Up and Down methods outside of the constructor.
    Updated Up method to change FileSize column type to long.
    Updated Down method to revert FileSize column type to string.

--- FileServer.API/Migrations/20220920180035_JFileV2ToDb.cs
+++ FileServer.API/Migrations/20220920180035_JFileV2ToDb.cs
@@ -2,32 +2,31 @@
 
 #nullable disable
 
-namespace FileServer.API.Migrations
-{
-    public partial class JFileV2ToDb : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<long>(
-                name: "FileSize",
-                table: "Files",
-                type: "bigint",
-                nullable: false,
-                defaultValue: 0L,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
-        }
+namespace FileServer.API.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "FileSize",
-                table: "Files",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(long),
-                oldType: "bigint");
-        }
+public partial class JFileV2ToDb : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<long>(
+            name: "FileSize",
+            table: "Files",
+            type: "bigint",
+            nullable: false,
+            defaultValue: 0L,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "FileSize",
+            table: "Files",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(long),
+            oldType: "bigint");
     }
 }
